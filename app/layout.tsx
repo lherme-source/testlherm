cee0b7d9c027258340661b559fc2768328c61812
lherme-source <147443678+lherme-source@users.noreply.github.com>
2025-10-28 19:20:00 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-export const metadata = { title: 'WJ — Protótipo WhatsApp UI', description: 'Protótipo com Next.js + Tailwind' }
+export const metadata = { title: 'WJ — Protótipo WhatsApp UI By Guilherme Spode', description: 'Protótipo com Next.js + Tailwind' }
 export default function RootLayout({ children }: { children: React.ReactNode }){
   return (<html lang="pt-br"><body>{children}</body></html>)
 }
